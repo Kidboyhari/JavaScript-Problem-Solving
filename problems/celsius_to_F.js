dbd195d74de544220586d6b4b19c619e3973f8ea
HariKrishna G <kidboyhari@gmail.com>
2026-07-03 14:31:05 +0530

    Celsius to Fahreheit

---
+++ problems/celsius_to_F.js
@@ -0,0 +1,6 @@
+function celsiustoFahrenheit(celsius){
+    let Fahrenheit = (celsius * 9/5 + 32);
+    return Fahrenheit;
+}
+console.log(celsiustoFahrenheit(25));
+console.log(celsiustoFahrenheit(0));
